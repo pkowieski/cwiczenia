e31670fc4736d77a56583626dd31a79d37288df3
pkowieski <kowieski.pawel@gmial.com>
2018-01-11 11:23:15 +0100

    tekst poprawianie

--- tekst.c
+++ tekst.c
@@ -3,11 +3,11 @@
 int main(){
 
 	char *Napis="Waniki";
-//	*(Napis+1)='y';  tu jest błąd!
+//	*(Napis+1)='y';  Tu jest błąd! Nie można poprawić tak zdefioniowanego tekstu
 
 	printf("%s\n",Napis);
 
-	char Tekst[20]={"Pymołka"};
+	char Tekst[20]={"Pymołka"}; // Gdy tekst jest tak zdefioniowany można poprawiać. 
 	Tekst[1]='o';
 	*(Tekst+3)='y';
 
