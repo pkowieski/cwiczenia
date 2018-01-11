cdbb08a507fcb4317be22c72a9ba402d53c62073
pkowieski <kowieski.pawel@gmial.com>
2018-01-11 12:43:34 +0100

    Zliczanie malych liter w ciagu znaków

---
+++ zliczanie_malych_liter.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+
+	const char ciag_znakow[100]="Ala ma kotka";
+	int histogram[26]={0};
+	int licznik=0;
+	char znak;
+
+	while(ciag_znakow[licznik]!=0){
+		znak=ciag_znakow[licznik];
+		if((znak>='a') && (znak<='z')){
+			++histogram[znak - 'a'];
+		}
+		++licznik;
+	}
+	for(int x=0;x<26;x++){
+		printf("%c - %d \n",'a'+x,histogram[x]);
+		}
+	return 0;
+}
